35d4d9a99df04a8f748fa54f2ab1bc5f09933bc8
kimdohyung <pdohyung@gmail.com>
2023-12-13 10:53:34 +0900

    feat(OutputView): 최종 우승자를 출력한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,6 +9,7 @@ import static java.text.MessageFormat.*;
 public class OutputView {
     private static final String PRINT_START_RESULT_MESSAGE = "실행 결과";
     private static final String PRINT_RACE_RESULT_FORMAT = "{0} : {1}";
+    private static final String PRINT_WINNERS_FORMAT = "최종 우승자 : {0}";
 
     public void printStartResult() {
         System.out.println(PRINT_START_RESULT_MESSAGE);
@@ -19,5 +20,9 @@ public class OutputView {
             System.out.println(format(PRINT_RACE_RESULT_FORMAT, car.getName(), raceResult.get(car)));
         }
         System.out.println();
+    }
+
+    public void printWinners(String winners) {
+        System.out.println(format(PRINT_WINNERS_FORMAT, winners));
     }
 }
