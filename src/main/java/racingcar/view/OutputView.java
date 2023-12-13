0f68d0f5369c137708000b4a37d5f451d2be7ce2
kimdohyung <pdohyung@gmail.com>
2023-12-13 10:48:03 +0900

    feat(OutputView): 각 경주의 실행 결과를 출력하는 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.Map;
+
+import static java.text.MessageFormat.*;
+
+public class OutputView {
+    private static final String PRINT_START_RESULT_MESSAGE = "실행 결과";
+    private static final String PRINT_RACE_RESULT_FORMAT = "{0} : {1}";
+
+    public void printStartResult() {
+        System.out.println(PRINT_START_RESULT_MESSAGE);
+    }
+
+    public void printRaceResult(Map<Car, String> raceResult) {
+        for (Car car : raceResult.keySet()) {
+            System.out.println(format(PRINT_RACE_RESULT_FORMAT, car.getName(), raceResult.get(car)));
+        }
+        System.out.println();
+    }
+}
