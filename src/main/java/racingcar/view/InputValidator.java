27a771efb6891aaa012e097896424b5ee496b229
kimdohyung <pdohyung@gmail.com>
2023-12-13 10:27:03 +0900

    feat(InputView): 시도할 횟수 입력 기능 추가

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -4,12 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputValidator {
+    private static final String INVALID_NUMBER_FORMAT_MESSAGE = "숫자 형식 입력이 아닙니다.";
+
     public static List<String> validateInputCarNames(String input) {
         return splitInputByComma(input);
+    }
+
+    public static int validateInputNumberRaces(String input) {
+        return convertStringToInteger(input);
     }
 
     private static List<String> splitInputByComma(String input) {
         return Arrays.stream(input.split(","))
                 .toList();
+    }
+
+    private static int convertStringToInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_MESSAGE);
+        }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,15 @@ import java.util.List;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUMBER_RACES_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         return InputValidator.validateInputCarNames(Console.readLine());
+    }
+
+    public int inputNumberRaces() {
+        System.out.println(INPUT_NUMBER_RACES_MESSAGE);
+        return InputValidator.validateInputNumberRaces(Console.readLine());
     }
 }
